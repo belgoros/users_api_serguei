174e46e2c24335cf873c1b5cfd0d67ad401c950c
belgoros <s.cambour@gmail.com>
2023-12-04 16:14:02 +0100

    Add graphql dependencies (absinthe)

--- mix.exs
+++ mix.exs
@@ -36,7 +36,10 @@ defmodule UsersApiSerguei.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe_phoenix, "~> 2.0"}
     ]
   end
 
