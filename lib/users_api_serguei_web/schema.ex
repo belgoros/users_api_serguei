14eccf22b3d146877c5940deef1704ab0db6201e
belgoros <s.cambour@gmail.com>
2023-12-09 11:55:15 +0100

    Refactor schema to use import_fields

--- lib/users_api_serguei_web/schema.ex
+++ lib/users_api_serguei_web/schema.ex
@@ -1,22 +1,10 @@
 defmodule UsersApiSergueiWeb.Schema do
   use Absinthe.Schema
-  import_types(__MODULE__.ContentTypes)
 
-  alias UsersApiSergueiWeb.Resolvers
+  import_types(__MODULE__.ContentTypes)
+  import_types(__MODULE__.Queries.UserQueries)
 
   query do
-    @desc "Get a user by id"
-    field :user, :user do
-      arg(:id, non_null(:id))
-      resolve(&Resolvers.Content.find_user/3)
-    end
-
-    @desc "List of users"
-    field :users, list_of(:user) do
-      arg(:likes_emails, :boolean)
-      arg(:likes_phone_calls, :boolean)
-      arg(:likes_faxes, :boolean)
-      resolve(&Resolvers.Content.list_users/3)
-    end
+    import_fields(:user_queries)
   end
 end
---
+++ lib/users_api_serguei_web/schema/queries/user_queries.ex
@@ -0,0 +1,21 @@
+defmodule UsersApiSergueiWeb.Schema.Queries.UserQueries do
+  use Absinthe.Schema.Notation
+
+  alias UsersApiSergueiWeb.Resolvers
+
+  object :user_queries do
+    @desc "List of users"
+    field :users, list_of(:user) do
+      arg(:likes_emails, :boolean)
+      arg(:likes_phone_calls, :boolean)
+      arg(:likes_faxes, :boolean)
+      resolve(&Resolvers.Content.list_users/3)
+    end
+
+    @desc "Get a user by id"
+    field :user, :user do
+      arg(:id, non_null(:id))
+      resolve(&Resolvers.Content.find_user/3)
+    end
+  end
+end
