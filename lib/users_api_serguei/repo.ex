723b83abf7d442527bd6971caee213b9cb787683
belgoros <s.cambour@gmail.com>
2024-01-03 12:18:32 +0100

    Add Repo/normalize_one/1 function

--- lib/users_api_serguei/repo.ex
+++ lib/users_api_serguei/repo.ex
@@ -3,7 +3,10 @@ defmodule UsersApiSerguei.Repo do
     otp_app: :users_api_serguei,
     adapter: Ecto.Adapters.Postgres
 
-
-
-  
+  def normalize_one(result) do
+    case result do
+      nil -> {:error, :not_found}
+      record -> {:ok, record}
+    end
+  end
 end
