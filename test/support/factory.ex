b1a7ad8831592fbaa58525a06693373691e3fe59
belgoros <s.cambour@gmail.com>
2024-01-04 12:19:41 +0100

    Remove fixtures and use factories

---
+++ test/support/factory.ex
@@ -0,0 +1,22 @@
+defmodule UsersApiSerguei.Factory do
+  # with Ecto
+  use ExMachina.Ecto, repo: UsersApiSerguei.Repo
+
+  alias UsersApiSerguei.Accounts.{User, Preference}
+
+  def user_factory() do
+    %User{
+      email: sequence(:email, &"user-#{&1}@example.com"),
+      name: sequence(:name, &"user-#{&1}")
+    }
+  end
+
+  def preference_factory() do
+    %Preference{
+      likes_emails: false,
+      likes_phone_calls: false,
+      likes_faxes: false,
+      user: build(:user)
+    }
+  end
+end
