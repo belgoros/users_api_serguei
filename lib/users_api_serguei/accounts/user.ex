00b00e763b857b95c9078b3fb07ab0c3a25f7f49
belgoros <s.cambour@gmail.com>
2024-01-17 15:35:42 +0100

    Add User/with_preferences/1

--- lib/users_api_serguei/accounts/user.ex
+++ lib/users_api_serguei/accounts/user.ex
@@ -2,11 +2,16 @@ defmodule UsersApiSerguei.Accounts.User do
   @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   schema "users" do
     field(:name, :string)
     field(:email, :string)
     has_many(:preferences, UsersApiSerguei.Accounts.Preference)
+  end
+
+  def with_preferences(query \\ User) do
+    join(query, :inner, [u], p in assoc(u, :preferences), as: :preference)
   end
 
   @available_attributes [:name, :email]
