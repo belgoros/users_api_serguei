47d58f60141c5c6af2fcb570aea7fd2705e7abdf
belgoros <s.cambour@gmail.com>
2024-02-06 16:18:23 +0100

    Use String.existing_atom/1

--- lib/users_api_serguei_web/graphql/resolvers/hits_resolver.ex
+++ lib/users_api_serguei_web/graphql/resolvers/hits_resolver.ex
@@ -3,10 +3,22 @@ defmodule UsersApiSergueiWeb.Graphql.Resolvers.HitsResolver do
 
   alias UsersApiSerguei.HitsCounter
 
-  def total_hits(_parent, %{key: request_to_store}, _resolution) do
-    request_key = String.to_atom(request_to_store)
+  @accepted_request_types [:create_user, :list_users, :find_user, :create_user, :update_user]
+  @accepted_request_type_strings Enum.map(@accepted_request_types, &to_string/1)
+
+  def total_hits(_parent, %{key: request_to_store}, _resolution)
+      when request_to_store in @accepted_request_type_strings do
+    request_key = String.to_existing_atom(request_to_store)
     hits_number = HitsCounter.resolver_hits(request_key)
 
     {:ok, hits_number}
+  end
+
+  def total_hits(_parent, %{key: request_to_store}, _resolution) do
+    try do
+      throw(request_to_store)
+    catch
+      request_to_store -> {:error, "Unknown value for request key: #{request_to_store}"}
+    end
   end
 end
